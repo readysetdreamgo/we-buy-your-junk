d782312ca14fda7dd2038a5457d67c024534086e
readysetdreamgo <akai@akaimade.com>
2026-01-16 21:03:27 -0500

    Configure Keystatic for GitHub cloud storage

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,8 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: 'readysetdreamgo/we-buy-your-junk',
   },
   ui: {
     brand: {
